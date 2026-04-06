80ebbd1a10ae8f6bb355e8d8519fe5571e297104
Abdullah Ayman <abdulluhayman@gmail.com>
2026-04-06 22:49:16 +0200

    Add the identity extensions to the repo so it doesn't crash the workflow

---
+++ Utilities/IdentityExtensions.cs
@@ -0,0 +1,51 @@
+using EgyptOnline.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+
+namespace EgyptOnline.Utilities
+{
+    public static class IdentityExtensions
+    {
+        public static async Task SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            if (!await roleManager.RoleExistsAsync(Roles.Admin))
+                await roleManager.CreateAsync(new IdentityRole(Roles.Admin));
+
+            if (!await roleManager.RoleExistsAsync(Roles.User))
+                await roleManager.CreateAsync(new IdentityRole(Roles.User));
+        }
+
+        public static async Task SeedAdmin(UserManager<User> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            string adminEmail = configuration["AdminSettings:Email"] ;
+            string adminPassword = configuration["AdminSettings:Password"] ;
+
+            var admin = await userManager.FindByEmailAsync(adminEmail);
+            if (admin == null)
+            {
+                admin = new User
+                {
+                    UserName = "admin",
+                    Email = adminEmail,
+                    FirstName = "Super",
+                    LastName = "Admin",
+                    Governorate = "Cairo",
+                    City = "Cairo"
+                };
+
+                var result = await userManager.CreateAsync(admin, adminPassword);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, Roles.Admin);
+                }
+            }
+            else
+            {
+                if (!await userManager.IsInRoleAsync(admin, Roles.Admin))
+                {
+                    await userManager.AddToRoleAsync(admin, Roles.Admin);
+                }
+            }
+        }
+    }
+}
